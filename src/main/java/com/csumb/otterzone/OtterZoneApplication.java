ab5495b4214b3ad0e70d0165bf8574f233eea55a
NixMelos <yavikkapadia@gmail.com>
2022-01-31 23:51:38 -0800

    Uploaded Project files.

---
+++ src/main/java/com/csumb/otterzone/OtterZoneApplication.java
@@ -0,0 +1,14 @@
+package com.csumb.otterzone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class OtterZoneApplication
+{
+
+   public static void main(String[] args)
+   {
+      SpringApplication.run(OtterZoneApplication.class, args);
+   }
+
+}
