4c658beb2928c1fd9136345cf1ea6d9c6483c7e6
Nix Melos <yavikkapadia@gmail.com>
2022-02-02 21:46:36 -0800

    updated Project file Cst363Application.java

--- src/main/java/com/csumb/cst363/Cst363Application.java
+++ src/main/java/com/csumb/cst363/Cst363Application.java
@@ -3,12 +3,12 @@ package com.csumb.cst363;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication public class OtterZoneApplication
+@SpringBootApplication public class Cst363Application
 {
 
    public static void main(String[] args)
    {
-      SpringApplication.run(OtterZoneApplication.class, args);
+      SpringApplication.run(Cst363Application.class, args);
    }
 
 }
