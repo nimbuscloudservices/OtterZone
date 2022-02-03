1cd0cdcc7fe9b172d225594c02c35c0953273433
Nix Melos <yavikkapadia@gmail.com>
2022-02-02 21:17:05 -0800

    moved OtterZoneApplication file to cst363 package

--- src/main/java/com/csumb/cst363/FDAReport.java
+++ src/main/java/com/csumb/cst363/FDAReport.java
@@ -1,4 +1,25 @@
 package com.csumb.cst363;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class FDAReport {
+   public static void main(String[] args) {
+
+      String url = "jdbc:mysql://http://ec2-13-56-197-130.us-west-1.compute.amazonaws.com:3306/OtterZone";
+      String user = "root";
+      String password = "ToInfinityAndBeyond";
+      try (Connection con = DriverManager.getConnection(url, user, password); ) {
+         con.setAutoCommit(false);
+         String sql = "Select";
+
+
+
+         con.commit();
+
+      } catch (SQLException ex) {
+         ex.printStackTrace();
+      }
+
+   }
 }
--- src/main/java/com/csumb/cst363/OtterZoneApplication.java
+++ src/main/java/com/csumb/cst363/OtterZoneApplication.java
@@ -1,4 +1,4 @@
-package com.csumb.otterzone;
+package com.csumb.cst363;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
