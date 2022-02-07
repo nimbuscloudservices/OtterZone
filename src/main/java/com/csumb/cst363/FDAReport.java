5ebaf4c7cc17e984ed252f488126c48d9eb417ff
Yavik Kapadia <yavikkapadia@gmail.com>
2022-02-06 22:57:10 -0800

    Removed incomplete code block

--- src/main/java/com/csumb/cst363/FDAReport.java
+++ src/main/java/com/csumb/cst363/FDAReport.java
@@ -6,20 +6,7 @@ import java.sql.SQLException;
 public class FDAReport {
    public static void main(String[] args) {
 
-      String url = "jdbc:mysql://http://ec2-13-56-197-130.us-west-1.compute.amazonaws.com:3306/OtterZone";
-      String user = "root";
-      String password = "ToInfinityAndBeyond";
-      try (Connection con = DriverManager.getConnection(url, user, password); ) {
-         con.setAutoCommit(false);
-         String sql = "Select ";
-
-
-
-         con.commit();
-
-      } catch (SQLException ex) {
-         ex.printStackTrace();
-      }
+      
 
    }
 }
