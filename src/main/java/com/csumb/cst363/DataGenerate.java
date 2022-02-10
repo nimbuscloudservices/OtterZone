e02ae469b4e5612e9213e62f0febb59a901e6c8b
Saul <saul.mendoza1995@gmail.com>
2022-02-10 10:05:46 -0800

    Update DataGenerate.java
    Cleaned up the file/code

--- src/main/java/com/csumb/cst363/DataGenerate.java
+++ src/main/java/com/csumb/cst363/DataGenerate.java
@@ -12,180 +12,236 @@ public class DataGenerate
 
    public static void main(String[] args) throws FileNotFoundException
    {
- 
-         int doc_id = 0;
+
+         int doctor_id;
+         int patient_id;
+         int prescription_id;
+
+         int first_three_ssn, middle_two_ssn, last_four_ssn;
+
          File name = new File("src/main/resources/names.txt");
          Scanner test = new Scanner(name);
-         
+
          Random gen = new Random();
-         String[] specialties = { "Internal Medicine", 
+
+         Random first_three_ssn_numbers = new Random(1000);
+         Random middle_two_ssn_numbers = new Random(100);
+         Random last_four_ssn_numbers = new Random(10000);
+
+         Random number_generator = new Random(16);
+
+         Random street_number = new Random(1000);
+
+         Random random_quanity = new Random(100);
+
+         String[] specialties = { "Internal Medicine",
                "Family Medicine", "Pediatrics", "Orthopedics",
-               "Dermatology",  "Cardiology", "Gynecology", 
+               "Dermatology",  "Cardiology", "Gynecology",
                "Gastroenterology", "Psychiatry", "Oncology" };
-         
-         Random ssn_generator = new Random(100000000); 
-         int A = 1;
-         int B = 99999999;
-         
-         Random number_generator = new Random(20); 
-         int C = 1;
-         int D = 19;
-         
+
+         String[] zipcodes = {"94016", "94102", "94103", "94104", "94110", "94117"};
+
+         String[] drug_names = {"Cerdelga", "Tegsedi", "Adderall", "Celexa", "Femara", "Briviact", "Ritalin",
+               "Tembexa", "Prolia" , "Otezla", "Dupixent", "Cibinqo", "Amevive", "Cleviprex" , "Juxtapid",
+               "Cholbam", "Dificid", "Edluar", "Fetzima", "Verzenio"};
+
          int[] birth_year = {1980, 1985, 1990, 1995, 2000};
-         
+
          SimpleDateFormat simpleDateFormat = new SimpleDateFormat("YYYY-MM-dd");
-         
+
+
          try(Connection con = DriverManager.getConnection("jdbc:mysql://ec2-13-56-197-130.us-west-1.compute.amazonaws.com:3306/OtterZoneDB", "root", "ToInfinityAndBeyond");)
          {
-            
+
+            //Generating 10 random doctors
             for(int i=0; i<10; i++)
             {
-               //Generating random specialty. 
-               
-               String random_specialty = specialties[gen.nextInt(specialties.length)];
-               
+               //Generating random specialty.
+               String random_specialty = specialties[i];
+
                //Generating random SSN number for doctor.
-               int doctor_ssn =   A + ssn_generator.nextInt(B-A+1);
-      
-               
-               //IO to get random name for doctor. 
+               first_three_ssn = first_three_ssn_numbers.nextInt(999-100+1) + 100;
+               middle_two_ssn = middle_two_ssn_numbers.nextInt(99-10+1) + 10;
+               last_four_ssn = last_four_ssn_numbers.nextInt(9999-1000+1) + 1000;
+               int doctor_ssn = Integer.valueOf(String.valueOf(first_three_ssn) + String.valueOf(middle_two_ssn) + String.valueOf(last_four_ssn));
+
+
+               //IO to get random name for doctor.
                test.hasNextLine();
                String doctor_name = test.nextLine();
-          
-               //Generating random date for doctor. 
+
+               //Generating random date for doctor.
                int random_birth_year = birth_year[gen.nextInt(birth_year.length)];
-               int random_number = C + number_generator.nextInt(D-C+1);
-               
+               int random_number = number_generator.nextInt(15-3+1) + 3;
+
                random_birth_year+=random_number;
-               
-               
+
                Calendar c = Calendar.getInstance();
                c.set(Calendar.YEAR,  random_birth_year);
                c.set(Calendar.DAY_OF_YEAR, 1);
                c.add(Calendar.DAY_OF_YEAR, gen.nextInt(365));
                Date dt = new Date(c.getTimeInMillis());
                String random_date = simpleDateFormat.format(dt);
-              
+
                PreparedStatement doctor = con.prepareStatement(
                      "insert into doctor(ssn, name, specialty, practice_since_year) values(?, ?, ?, ?)",
                      Statement.RETURN_GENERATED_KEYS);
-           
-            
-           
+
                doctor.setString(1, String.valueOf(doctor_ssn));
                doctor.setString(2, doctor_name);
                doctor.setString(3, random_specialty);
                doctor.setString(4, random_date);
-            
-            
+
                doctor.executeUpdate();
-            
+
                ResultSet rs = doctor.getGeneratedKeys();
-               int doctor_id;
                if (rs.next())
                {
-                  
+
                   doctor_id = rs.getInt(1);
-                  doc_id = doctor_id;
+
                 }
-               
-               
-               for(int j = 0; j < 100; j++)
-               {
-                  //Generating random SSN number for patient.
-                  int patient_ssn =   A + ssn_generator.nextInt(B-A+1);
-         
-                  //IO to get random name for patient. 
-                  test.hasNextLine();
-                  String patient_name = test.nextLine();
-             
-                  //Generating random date for patient. 
-                  int patient_birth_year = birth_year[gen.nextInt(birth_year.length)];               
-                  int patient_number = C + number_generator.nextInt(D-C+1);
-                  
-                  patient_birth_year+=patient_number;
-               
-                  Calendar patient_birthdate = Calendar.getInstance();
-                  patient_birthdate.set(Calendar.YEAR,  patient_birth_year);
-                  patient_birthdate.set(Calendar.DAY_OF_YEAR, 1);
-                  patient_birthdate.add(Calendar.DAY_OF_YEAR, gen.nextInt(365));
-                  Date patient_dt = new Date(c.getTimeInMillis());
-                  String random_dt = simpleDateFormat.format(patient_dt);
-                  
-                  //Generating random address for patient.
-                  Random street_number = new Random(1000); 
-                  int E = 1;
-                  int F = 999;
-                  int street =   E + street_number.nextInt(F-E+1);
-                  
-                  //IO to get random street. 
-                  File street_name = new File("src/main/resources/streets.txt");
-                  Scanner st = new Scanner(street_name);
-                  st.hasNextLine();
-                  String st_name = st.nextLine();
-                  
-                  String[] zipcodes = {"94016", "94102", "94103", "94104", "94110", "94117"};
-                  String random_zipcode = zipcodes[gen.nextInt(zipcodes.length)];
-                  
-                  PreparedStatement patient = con.prepareStatement(
-                        "insert into patient(ssn, name, birthdate, street, city, state, zipcode, primaryName) values(?, ?, ?, ?, ?, ?, ?, ?)",
-                        Statement.RETURN_GENERATED_KEYS);
-              
-               
-              
-                  patient.setString(1, String.valueOf(patient_ssn));
-                  patient.setString(2, patient_name);
-                  patient.setString(3, random_dt);
-                  patient.setString(4, String.valueOf(street) + " " + st_name);
-                  patient.setString(5, "San Francisco");
-                  patient.setString(6, "CA");
-                  patient.setString(7, random_zipcode);
-                  patient.setString(8, doctor_name);
-               
-               
-                  patient.executeUpdate();
-               
-                  ResultSet rs_patient = patient.getGeneratedKeys();
-                  int patient_id;
-                  if (rs_patient.next()) patient_id = rs_patient.getInt(1);
-                  st.close();
-                  
-                  for(int k = 0; k< 5; k++)
-                  {
-                     String[] drug_names = {"Cerdelga", "Tegsedi", "Adderall", "Celexa", "Femara", "Briviact", "Ritalin", 
-                           "Tembexa", "Prolia" , "Otezla", "Dupixent", "Cibinqo", "Amevive", "Cleviprex" , "Juxtapid", 
-                           "Cholbam", "Dificid", "Edluar", "Fetzima", "Verzenio"};
-                     String random_drug = drug_names[gen.nextInt(drug_names.length)];
-                     
-                     Random random_quanity = new Random(100); 
-                     int G = 99;
-                     int quantity =   E + street_number.nextInt(G-E+1);
-                     PreparedStatement prescription = con.prepareStatement(
-                           "insert into Prescription(drugName, quantity, patient_ssn, patientName, doctor_ssn, doctorName, Doctor_id) values(?, ?, ?, ?, ?, ?, ?)",
-                           Statement.RETURN_GENERATED_KEYS);
-                     
-                     prescription.setString(1, random_drug);
-                     prescription.setInt(2, quantity);
-                     prescription.setString(3, String.valueOf(patient_ssn));
-                     prescription.setString(4, patient_name);
-                     prescription.setString(5, String.valueOf(doctor_ssn));
-                     prescription.setString(6, doctor_name);
-                     prescription.setInt(7, doc_id);
-                     
-                     prescription.executeUpdate();
-                     
-                     ResultSet rs_prescription = prescription.getGeneratedKeys();
-                     int prescription_id;
-                     if (rs_prescription.next()) prescription_id = rs_prescription.getInt(1);
-                     
-                  }
-               }
-               
+
             }
-          
+
+            File street_name = new File("src/main/resources/streets.txt");
+            Scanner st = new Scanner(street_name);
+
+            //Generating 1000 random patients
+            for(int j = 0; j < 1000; j++)
+            {
+               //Generating random SSN number for patient.
+               first_three_ssn = first_three_ssn_numbers.nextInt(999-100+1) + 100;
+               middle_two_ssn = middle_two_ssn_numbers.nextInt(99-10+1) + 10;
+               last_four_ssn = last_four_ssn_numbers.nextInt(9999-1000+1) + 1000;
+               int patient_ssn = Integer.valueOf(String.valueOf(first_three_ssn) + String.valueOf(middle_two_ssn) + String.valueOf(last_four_ssn));
+
+               //IO to get random name for patient.
+               test.hasNextLine();
+               String patient_name = test.nextLine();
+
+               //Generating random date for patient.
+               int patient_birth_year = birth_year[gen.nextInt(birth_year.length)];
+               int patient_number = number_generator.nextInt(15-2+1) + 2;
+
+               patient_birth_year+=patient_number;
+
+               Calendar patient_birthdate = Calendar.getInstance();
+               Calendar current_time = Calendar.getInstance();
+               patient_birthdate.set(Calendar.YEAR,  patient_birth_year);
+               patient_birthdate.set(Calendar.DAY_OF_YEAR, 1);
+               patient_birthdate.add(Calendar.DAY_OF_YEAR, gen.nextInt(365));
+               Date patient_dt = new Date(patient_birthdate.getTimeInMillis());
+               Date current_dt = new Date(current_time.getTimeInMillis());
+               String random_dt = simpleDateFormat.format(patient_dt);
+
+
+
+               String doctor_ssn = null;
+               String doctor = null;
+
+
+               current_time.add(Calendar.YEAR, -17);
+               System.out.println(current_time.get(Calendar.YEAR) + " " + patient_birthdate.get(Calendar.YEAR));
+
+               doctor_id = 0;
+
+               if(patient_birthdate.get(Calendar.YEAR) >= current_time.get(Calendar.YEAR))
+               {
+                  PreparedStatement ps = con.prepareStatement( "select id, ssn, name from doctor" +
+                        " where specialty = ?");
+                     ps.setString(1, "Pediatrics");
+
+                     ResultSet rs = ps.executeQuery();
+                     while (rs.next()) {
+                        doctor_id = rs.getInt(1);
+                        doctor_ssn = rs.getString(2);
+                        doctor = rs.getString(3);
+                     }
+               }
+               else
+               {
+                  PreparedStatement ps = con.prepareStatement( "select id, ssn, name from doctor" +
+                        " where specialty = ? or specialty = ? ORDER BY RAND() LIMIT 1" );
+                     ps.setString(1, "Internal Medicine");
+                     ps.setString(2, "Family Medicine");
+
+                     ResultSet rs = ps.executeQuery();
+
+                     while (rs.next()) {
+                        doctor_id = rs.getInt(1);
+                        doctor_ssn = rs.getString(2);
+                        doctor = rs.getString(3);
+                     }
+               }
+               //Generating random address for patient.
+               int street = street_number.nextInt(999-10+1) + 10;
+
+               //IO to get random street.
+               String st_name;
+               if(st.hasNextLine())
+               {
+                   st_name = st.nextLine();
+               }
+               else
+               {
+                  st.close();
+                  street_name = new File("src/main/resources/streets.txt");
+                  st = new Scanner(street_name);
+                  st.hasNextLine();
+                  st_name = st.nextLine();
+               }
+
+               String random_zipcode = zipcodes[gen.nextInt(zipcodes.length)];
+
+               PreparedStatement patient = con.prepareStatement(
+                     "insert into patient(ssn, name, birthdate, street, city, state, zipcode, primaryName) values(?, ?, ?, ?, ?, ?, ?, ?)",
+                     Statement.RETURN_GENERATED_KEYS);
+
+               patient.setString(1, String.valueOf(patient_ssn));
+               patient.setString(2, patient_name);
+               patient.setString(3, random_dt);
+               patient.setString(4, String.valueOf(street) + " " + st_name);
+               patient.setString(5, "San Francisco");
+               patient.setString(6, "CA");
+               patient.setString(7, random_zipcode);
+               patient.setString(8, doctor);
+
+               patient.executeUpdate();
+
+               ResultSet rs_patient = patient.getGeneratedKeys();
+               if (rs_patient.next())
+               {
+                  patient_id = rs_patient.getInt(1);
+               }
+
+               for(int k = 0; k < 5; k++)
+               {
+                  String random_drug = drug_names[gen.nextInt(drug_names.length)];
+
+                  int quantity =  street_number.nextInt(99-1+1)+1;
+                  PreparedStatement prescription = con.prepareStatement(
+                        "insert into Prescription(drugName, quantity, patient_ssn, patientName, doctor_ssn, doctorName, Doctor_id) values(?, ?, ?, ?, ?, ?, ?)",
+                        Statement.RETURN_GENERATED_KEYS);
+
+                  prescription.setString(1, random_drug);
+                  prescription.setInt(2, quantity);
+                  prescription.setString(3, String.valueOf(patient_ssn));
+                  prescription.setString(4, patient_name);
+                  prescription.setString(5, String.valueOf(doctor_ssn));
+                  prescription.setString(6, doctor);
+                  prescription.setInt(7, doctor_id);
+
+                  prescription.executeUpdate();
+
+                  ResultSet rs_prescription = prescription.getGeneratedKeys();
+
+                  if (rs_prescription.next()) prescription_id = rs_prescription.getInt(1);
+
+               }
+            }
          }catch (SQLException e) {
-            System.out.print("Error");
+            System.out.print("Error " + e.getMessage());
          }
    }
- 
 }
